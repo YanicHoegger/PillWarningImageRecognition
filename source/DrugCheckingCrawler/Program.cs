edca77616dfcc5fb2a82cd3211e27f83324543a6
YanicHoegger <yanic.hoegger@gmx.ch>
2020-05-04 13:47:42 +0200

    Download Pdfs

--- source/DrugCheckingCrawler/Program.cs
+++ source/DrugCheckingCrawler/Program.cs
@@ -2,11 +2,22 @@
 
 namespace DrugCheckingCrawler
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main()
         {
+
+            var collector = new PdfCollector();
+
+            var index = 1;
+            var writer = new PdfFileWriter(@"C:\Users\Yanic\Desktop\Temp");
+            foreach(var file in collector.GetPdfs())
+            {
+                writer.WriteFile(index++.ToString(), file);
+            }
+
             Console.WriteLine("Hello World!");
+
         }
     }
 }
